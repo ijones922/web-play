c6aa66a0f2accd39d68c5c73d63db8b98fd6907b
ian jones <ijones92288@gmail.com>
2013-03-06 23:59:05 -0500

    added new parallax template, archived first template

--- Ian/js/theme.js
+++ Ian/js/theme.js
@@ -1,32 +1,8 @@
 
-$(function () {
-$(window).scroll(function(){
-// add navbar opacity on scroll
-if ($(this).scrollTop() > 100) {
-$(".navbar.navbar-fixed-top").addClass("scroll");
-} else {
-$(".navbar.navbar-fixed-top").removeClass("scroll");
-}
-// global scroll to top button
-if ($(this).scrollTop() > 300) {
-$('.scrolltop').fadeIn();
-} else {
-$('.scrolltop').fadeOut();
-}
-});
-$('#myCarousel').carousel('cycle');
-// scroll back to top btn
-$('.scrolltop').click(function(){
-$("html, body").animate({ scrollTop: 0 }, 700);
-return false;
-});
-// scroll navigation functionality
-$('.scroller').click(function(){
-var section = $($(this).data("section"));
-var top = section.offset().top;
-$("html, body").animate({ scrollTop: top }, 700);
-return false;
-});
-// FAQs
-var $faqs = $("#faq .faq");
-$faqs.click(function () {
+// carousel demo
+!function ($) {
+$('#myCarousel').carousel()
+}(window.jQuery)
+!function ($) {
+$('#myCarousel2').carousel()
+}(window.jQuery) 
